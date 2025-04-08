cb238aec20cbff77c87a8ffd0b9d354598955b26
BrandonLlivisaca <brandonllivisaca13@gmail.com>
2025-04-07 23:11:57 -0500

    first commit

---
+++ src/main/java/com/service/app/ServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAppApplication {
+	// first commit
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAppApplication.class, args);
+	}
+
+}
